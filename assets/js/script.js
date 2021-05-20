441611663126bdd31b67a23988c49e6adb6a7a00
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-20 19:42:44 -0400

    psuedo coded js file

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// creating a daily planner
+
+
+
+// when opening planner current day is displayed at top of calendar
+
+
+
+// create time blocks for standard business hours
+
+
+
+// make each time block color-coded for past, present, future
+
+
+
+// when click on time block can add event
+
+
+
+// when clicking save button for event created it saves in local storage
+
+
+
+// when page is refreshed make sure the saved events persists
