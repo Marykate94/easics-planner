9556e0e69c6ab93840c74000a8544dabebe1d018
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-21 18:13:41 -0400

    trying to add a save button connected to input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 // creating a daily planner
-
-
-
 // when opening planner current day is displayed at top of calendar
 var todayDate = moment().format('dddd, MMMM Do');
 console.log(todayDate);
@@ -12,18 +9,28 @@ document.getElementById("currentDay").innerHTML = todayDate;
 // }
 
 // create time blocks for standard business hours in HTML
-
+// created in HTML
 
 // make each time block color-coded for past, present, future
 
 
 // when click on time block can add event
-
-
+// 
 
 // when clicking save button for event created it saves in local storage
-var input = document.getElementById("saveServer");
-localStorage.setItem("server", input.val());
+// create button
+// create edit button
+var inputEl = document.querySelector("description");
+var saveBtn = inputEl.getAttribute("save-button");
+var saveButtonEl = document.createElement("button");
+saveButtonEl.textContent = "Save";
+saveButtonEl.className = "saveBtn";
+saveButtonEl.setAttribute("save-button", saveBtn);
+
+actionContainerEl.appendChild(saveButtonEl);
+// create local storage 
+// var input = document.getElementById("saveServer");
+// localStorage.setItem("server", input.val());
 
 
 // when page is refreshed make sure the saved events persists
