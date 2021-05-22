596f55dd2b5072aa02343062ceaad0d813ec543c
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-22 12:35:18 -0400

    added input connected to save button block

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,25 +21,30 @@ document.getElementById("currentDay").innerHTML = todayDate;
 // create button
 // create edit button
 
-// $(".saveButton").click(function() {
-//     // grab id's for text input elements
-//     $("#input1, #input2, #input3, #input4, #input5, #input6, #input7, #input8, #input9");
-// });
-// console.log(".saveButton"); <-- jQuery method
+$(".saveButton").click(function() {
+     // grab id's for text input elements
+    //  $("#input1, #input2, #input3, #input4, #input5, #input6, #input7, #input8, #input9");
+    var currentID = $(this).attr("id").split("-")[1]
+    var userPlan = $(`#input-${currentID}`).val() //$("#input-"+currentID)
+    console.log(`This is time block ${currentID} and ${userPlan}`)
+    
 
-function captureSaveEvent(event) {
-    console.log(event);
-    //variable parent div is equal to event.target.parentdiv save variable of description event.target.parentdiv.description.innertext
-    //look for the input type in the console might need to save whole parent div as a variable
-    var storeDescription = localStorage.getItem(".description",input);
-    var parentDiv = event.target.container;
-    localStorage.setItem(".description",input.val());
-}
-var saveBtn = document.getElementsByClassName("saveButton");
-for (var i = 0; i < saveBtn.length; i++) {
-    saveBtn[i].addEventListener("click", captureSaveEvent);
+ });
+//  console.log(".saveButton"); 
 
-}
+// function captureSaveEvent(event) {
+//     console.log(event);
+//     //variable parent div is equal to event.target.parentdiv save variable of description event.target.parentdiv.description.innertext
+//     //look for the input type in the console might need to save whole parent div as a variable
+//     var storeDescription = localStorage.getItem(".description",input);
+//     var parentDiv = event.target.container;
+//     localStorage.setItem(".description",input.val());
+// }
+// var saveBtn = document.getElementsByClassName("saveButton");
+// for (var i = 0; i < saveBtn.length; i++) {
+//     saveBtn[i].addEventListener("click", captureSaveEvent);
+  //document.getElementById("input-9").value
+// }
 
 
 // var inputEl = document.querySelector("description");
